cec111333913ca2e6a75590454c77d35282e1cb6
ParkSsoj <inhist44@gmail.com>
2025-09-06 17:54:16 +0900

    내 변경사항 저장

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";    // named export
 // React 18부터는 react-dom에서 render을 직접 쓰지 않고 createRoot를 써야 한다. 현재 버전 react@19.1.1
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import StartView from "./views/StartView";  
+import ConfigView from "./views/ConfigurationView";
 import GameView from "./views/GameView";
 import ManualView from "./views/ManualView";
 import ResultView from "./views/ResultView";
@@ -14,6 +15,7 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/start" element={<StartView />} />
+        <Route path="/config" element={<ConfigView />} />
         <Route path="/game" element={<GameView />} />
         <Route path="/manual" element={<ManualView />} />
         <Route path="/result" element={<ResultView />} />
