bb716cc95581f8a754d46578a3249bb01bc82524
s0-yeon <csi10186@gmail.com>
2025-09-08 14:54:48 +0900

    debug-nextround-double-click

--- src/renderer/views/ResultView.jsx
+++ src/renderer/views/ResultView.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { useNavigate, useLocation } from "react-router-dom";
-import { gameController } from "../controller/GameController"; // ✅ 인스턴스 import
+import { gameController } from "../controllers/GameController"; // ✅ 인스턴스 import
 import { useGameController } from "../hooks/useGameController";
 
 export default function ResultView() {
