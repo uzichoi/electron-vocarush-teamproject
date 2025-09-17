6bcb0aa904c1094d50c9c215f570d3093159a5c2
uzichoi <anonymous@example.com>
2025-09-17 19:57:35 +0900

    add createWindows.js

--- src/main/createWindows.js
+++ src/main/createWindows.js
@@ -7,7 +7,7 @@ function createWindow() {
   win = new BrowserWindow({
     title: "VocaRush",
     resizable: true,     // 윈도우 크기 조절 비허용
-    fullscreen: true,     // 윈도우 전체 화면 활성화
+    fullscreen: false,     // 윈도우 전체 화면 활성화
     alwaysOnTop: false,    // 윈도우를 항상 다른 창들 위에 표시
     webPreferences: {
         nodeIntegration: true,      // Node.js require 허용
