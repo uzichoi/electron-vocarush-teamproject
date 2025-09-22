0cd2ecd10135c8a474b5b3c7b7e3be9879660949
s0-yeon <csi10186@gmail.com>
2025-09-22 19:44:59 +0900

    f

--- src/renderer/models/SoundManager.js
+++ src/renderer/models/SoundManager.js
@@ -12,7 +12,7 @@ class SoundManager {
     this.sounds = {
             // clickPop: new Audio(`file://${path.join(basePath, "clickPop.mp3")}`),
       clickPop : new Howl({
-      src: ["D:VocaRush-team/electron-vocarush-teamproject/src/renderer/assets/soundEffect/clickPop.wav"],
+      src: ["/home/pi/VocaRush-team/electron-vocarush-teamproject/src/renderer/assets/soundEffect/clickPop.wav"],
     })
     };
   }
